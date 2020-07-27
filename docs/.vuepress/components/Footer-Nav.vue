81309419a29a68e8d5705a7309a634d2f6f15852
Matt <mpau548@aucklanduni.ac.nz>
2020-07-27 13:16:16 +1200

    Tweaked styles. Darkness woo! Added beta version of footer nav component

--- docs/.vuepress/components/Footer-Nav.vue
+++ docs/.vuepress/components/Footer-Nav.vue
@@ -1,52 +1,94 @@
 <template>
-  <div>
-    <!-- <ul>
-      <li v-for="page in sortedPages">
-        <b>
-          {{page.categories}}
-        </b>
-        -- {{page.title}}. {{page.path}}.
-        </li>
-    </ul> -->
-    <div v-for="(cat, i) in footerCategories">
-      {{footerCategories[i]}}
-      <ul>
-        <li v-for="page in cat">
+  <div class="footer-navigation">
+    <h2 class="footer-navigation-title">Pages</h2>
+    <div
+      v-for="(cat, i) in footerCategories"
+      class="categories"
+    >
+      <div class="category-title">
+        {{cat}}
+      </div>
+      <div class="pages">
+        <a
+          v-for="page in footerCategoriesObj[cat]"
+          class="page-link"
+          :href="page.path"
+        >
           {{page.title}}
-        </li>
-      </ul>
+        </a>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
   computed: {
-    sortedPages() {
-      let sortedPages = this.$site.pages.map(page => {
+    trimmedPageObjs() {
+      let trimmedPageObjs = this.$site.pages.map((page) => {
         return {
           categories: page.frontmatter && page.frontmatter.categories,
           title: page.title,
-          path: page.path
+          path: page.path,
         };
       });
 
-      return sortedPages;
+      return trimmedPageObjs;
     },
-    footerCategories() {
-      const footerCategories = this.sortedPages.reduce((categories, page) => {
+    footerCategoriesObj() {
+      const footerCategoriesObj = {};
+      this.trimmedPageObjs.forEach((page) => {
         if (page.categories && page.categories.length) {
-          page.categories.forEach(cat => {
-            console.log(categories);
-            if (categories[cat]) {
-              categories[cat].push(page);
+          page.categories.forEach((cat) => {
+            if (footerCategoriesObj[cat]) {
+              footerCategoriesObj[cat].push(page);
             } else {
-              categories[cat] = [page];
+              footerCategoriesObj[cat] = [page];
             }
-          }, {});
+          });
         }
       });
-      return footerCategories;
-    }
-  }
+      return footerCategoriesObj;
+    },
+    footerCategories() {
+      return Object.keys(this.footerCategoriesObj);
+    },
+  },
 };
 </script>
+
+<style scoped>
+.footer-navigation {
+  --border-colour: #ccc;
+  border: 1px solid var(--border-colour);
+}
+
+.footer-navigation-title {
+  font-size: 18px;
+  text-align: center;
+  padding: 10px 0;
+  margin: 0;
+  border: none;
+}
+
+.categories {
+  display: flex;
+  border-top: 1px solid var(--border-colour);
+}
+
+.category-title {
+  padding: 10px;
+  flex: 0 0 100px;
+  border-right: 1px solid var(--border-colour);
+  text-align: right;
+}
+
+.pages {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+  padding: 10px;
+}
+
+.page-link {
+}
+</style>
