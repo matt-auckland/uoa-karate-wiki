0f729020e0d077f0bf387bd1133b496bc065d960
Matt <mpau548@aucklanduni.ac.nz>
2020-07-27 13:37:02 +1200

    More nav footer tweaks

--- docs/.vuepress/components/Footer-Nav.vue
+++ docs/.vuepress/components/Footer-Nav.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="footer-navigation">
-    <h2 class="footer-navigation-title">Pages</h2>
+    <h2 class="footer-navigation-title">Wiki Pages</h2>
     <div
       v-for="(cat, i) in footerCategories"
       class="categories"
     >
       <div class="category-title">
-        {{cat}}
+        <a
+          v-if="footerCategoriesObj[cat] && footerCategoriesObj[cat].path"
+          :href="footerCategoriesObj[cat].path"
+        >
+          {{cat | categoryName}}
+        </a>
+        <span v-else>
+          {{cat | categoryName}}
+        </span>
       </div>
       <div class="pages">
         <a
-          v-for="page in footerCategoriesObj[cat]"
+          v-for="page in footerCategoriesObj[cat].pages"
           class="page-link"
           :href="page.path"
         >
@@ -22,6 +30,16 @@
 </template>
 <script>
 export default {
+  filters: {
+    categoryName(name) {
+      switch (name) {
+        case "Important":
+          return "Important Pages";
+        default:
+          return name;
+      }
+    },
+  },
   computed: {
     trimmedPageObjs() {
       let trimmedPageObjs = this.$site.pages.map((page) => {
@@ -35,18 +53,32 @@ export default {
       return trimmedPageObjs;
     },
     footerCategoriesObj() {
-      const footerCategoriesObj = {};
+      // we add Important here so it's the first category to show in the footer
+      const footerCategoriesObj = { Important: {} };
       this.trimmedPageObjs.forEach((page) => {
         if (page.categories && page.categories.length) {
           page.categories.forEach((cat) => {
-            if (footerCategoriesObj[cat]) {
-              footerCategoriesObj[cat].push(page);
-            } else {
-              footerCategoriesObj[cat] = [page];
+            // We don't want to display these categories right now
+            if (cat != "Hojo Undo" && cat != "Misc") {
+              if (footerCategoriesObj[cat] && footerCategoriesObj[cat].pages) {
+                footerCategoriesObj[cat].pages.push(page);
+              } else {
+                footerCategoriesObj[cat] = { pages: [page] };
+              }
+
+              // We don't want these categories to be links
+              if (cat != "Important" && cat != "Misc") {
+                if (page.path && page.path[page.path.length - 1] == "/") {
+                  console.log(page.path[page.path.length - 1]);
+                  footerCategoriesObj[cat].path = page.path;
+                  console.log(footerCategoriesObj[cat].path);
+                }
+              }
             }
           });
         }
       });
+      console.log(footerCategoriesObj);
       return footerCategoriesObj;
     },
     footerCategories() {
