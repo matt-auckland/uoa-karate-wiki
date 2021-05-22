ed243b1aebdd4455d225a5eb4ea2adf4373d8696
Matt <mpau548@aucklanduni.ac.nz>
2021-05-22 17:17:20 +1200

    Remove swipe to open sidebar because it's too buggy

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -2,8 +2,6 @@
   <div
     class="theme-container"
     :class="pageClasses"
-    @touchstart="onTouchStart"
-    @touchend="onTouchEnd"
   >
     <Navbar
       v-if="shouldShowNavbar"
@@ -127,25 +125,6 @@ export default {
       this.$emit('toggle-sidebar', this.isSidebarOpen)
     },
 
-    // side swipe
-    onTouchStart (e) {
-      this.touchStart = {
-        x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
-      }
-    },
-
-    onTouchEnd (e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
-      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-        if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
-        } else {
-          this.toggleSidebar(false)
-        }
-      }
-    }
   }
 }
 </script>
