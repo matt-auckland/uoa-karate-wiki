d9be888eeb90bb45b868d3f5c6e0b881134c4792
matt-auckland <mathewp.94@gmail.com>
2022-06-16 00:47:28 +1200

    Update Home page Update packagelock.

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -3,221 +3,213 @@
     class="home"
     aria-labelledby="main-title"
   >
-    <header class="hero">
-      <img
-        v-if="data.heroImage"
-        :src="$withBase(data.heroImage)"
-        :alt="data.heroAlt || 'hero'"
-      >
+    <div>
+      <h1>Welcome to the UoA Karate Wiki</h1>
 
-      <h1
-        v-if="data.heroText !== null"
-        id="main-title"
-      >
-        {{ data.heroText || $title || 'Hello' }}
-      </h1>
+      <blockquote>
+        Practise Daily and Protect Traditional Karate.
+      </blockquote>
 
-      <p
-        v-if="data.tagline !== null"
-        class="description"
-      >
-        {{ data.tagline || $description || 'Welcome to your VuePress site' }}
+      <p>
+        This is intended to be a reference resource for the study of Goju Ryu Karate and Karate in general. It's not an authoritative source (though it does strive to be as accurate as possible) and as such everything should be taken with a grain of salt. When in doubt, consult an instructor or senior member.
       </p>
 
-      <p
-        v-if="data.actionText && data.actionLink"
-        class="action"
-      >
-        <NavLink
-          class="action-button"
-          :item="actionLink"
-        />
-      </p>
-    </header>
+      <p>Click the boxes below to visit the various sections of the site, or use the search bar to explore.</p>
 
-    <div
-      v-if="data.features && data.features.length"
-      class="features"
-    >
-      <div
-        v-for="(feature, index) in data.features"
-        :key="index"
-        class="feature"
-      >
-        <h2>{{ feature.title }}</h2>
-        <p>{{ feature.details }}</p>
-      </div>
+      <h2>Useful Pages</h2>
+      <nav class="grid-cont">
+        <router-link
+          v-for="link in pageLinks"
+          :key="link.url"
+          class="page-link"
+          :to="link.url"
+        >
+          <span
+            class="icon"
+            v-html="link.icon"
+          ></span>
+          <h2 >{{link.title}}</h2>
+        </router-link>
+      </nav>
     </div>
-
-    <Content class="theme-default-content custom" />
-
-    <div
-      v-if="data.footer"
-      class="footer"
-    >
-      {{ data.footer }}
-    </div>
-    <Footer-Nav />
+    <Footer-Nav/>
   </main>
 </template>
 
 <script>
-import NavLink from "@theme/components/NavLink.vue";
-import FooterNav from "../../components/Footer-Nav.vue";
-
+import FooterNav from '../../components/Footer-Nav.vue'
 export default {
-  name: "Home",
-
-  components: { NavLink, FooterNav },
-
-  computed: {
-    data() {
-      return this.$page.frontmatter;
-    },
-
-    actionLink() {
-      return {
-        link: this.data.actionLink,
-        text: this.data.actionText,
-      };
-    },
+  components: { FooterNav },
+  data() {
+    return {
+      pageLinks: [
+        {
+          url: "/beginner-guide",
+          title: "Beginner's Guide",
+          icon: "初心者<wbr>ガイド",
+        },
+        {
+          url: "/goju-ryu",
+          title: "Goju Ryu Karate",
+          icon: "剛柔流<wbr>空手道",
+        },
+        {
+          url: "/kata/",
+          title: "Kata",
+          icon: "型",
+        },
+        {
+          url: "/bunkai/",
+          title: "Bunkai",
+          icon: "分解",
+        },
+        {
+          url: "/vocabulary",
+          title: "Vocabulary",
+          icon: "単語",
+        },
+        {
+          url: "/bunkai/renzoku-bunkai",
+          title: "Renzoku Bunkai",
+          icon: "連続<wbr>分解",
+        },
+        {
+          url: "/yakusoku-kumite/",
+          title: "Yakusoku Kumite",
+          icon: "約束<wbr>組手",
+        },
+        // {
+        //     url: "/junbi-undo/", title: "Junbi Undo (Warm Up Exercises) "
+        // icon: '準備<wbr>運動',
+        // },
+        {
+          url: "/hojo-undo/",
+          title: "Hojo Undo (Supplementary Exercises)",
+          icon: "幇助<wbr>運動",
+        },
+        // {
+        // url: "/kiai", title: "Kiai"
+        // icon: '気合い',
+        // },
+        // {
+        //  url: "/kakie", title: "Kakie"
+        // icon: 'カキエ',
+        // },
+        // {
+        // url: "/stances-and-footwork", title: "Footwork and Stances"
+        // icon: '立ち',
+        // },
+        // {
+        //     title: "Kumite (Sparring)" , url: "/kumite"/
+        // icon: '組手',
+        // },
+        // {
+        //   title: "Conditioning",
+        //   icon: "鍛え",
+        //   url: "/conditioning",
+        // },
+        {
+          url: "/etiquette",
+          title: "Bowing, sitting, and etiquette",
+          icon: "礼",
+        },
+        {
+          url: "ceremonies#opening-ceremony",
+          title: "Opening/Closing Ceremony",
+          icon: "儀式",
+        },
+        {
+          url: "ceremonies#dojo-kun",
+          title: "Dojo Kun",
+          icon: "道場訓",
+        },
+        {
+          url: "/grading",
+          title: "Grading Info",
+          icon: "帯試験",
+        },
+        {
+          url: "/kobudo",
+          title: "Weapons (Kobudo)",
+          icon: "古武道",
+        },
+        // {
+        //   url: "/training-tips",
+        //   title: "Training Tips",
+        //   icon: "",
+        // },
+        {
+          url: "/japanese",
+          title: "Learning Japanese",
+          icon: "日本語",
+        },
+        {
+          url: "/japanese#pronunciation",
+          title: "Japanese Pronunciation",
+          icon: "発音",
+        },
+      ],
+    };
   },
 };
 </script>
 
-<style lang="stylus">
-.home {
-  padding: $navbarHeight 2rem 0;
-  max-width: $homePageWidth;
-  margin: 0px auto;
+<style scoped>
+.grid-cont {
+  --size: 175px;
+  display: grid;
+  justify-content: center;
+  gap: 20px;
+  grid-template-columns: repeat(auto-fill, var(--size));
+  grid-auto-rows: var(--size);
+  list-style: none;
+}
+
+.page-link {
+  line-height: 1.3;
+  box-sizing: border-box;
+  border: 4px solid grey;
+  border-radius: 10px;
+  padding: 8px;
+  text-align: center;
+  text-decoration: none !important;
+  color: var(--text-color) !important;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.page-link:hover {
+  border-color: var(--accentColor);
+}
+
+.page-link h2 {
+  margin: 0;
+  font-size: 1.2em;
+  border: none;
+  text-decoration: underline;
+}
+
+.page-link .icon {
+  text-decoration: none;
+  font-size: 2em;
   display: block;
+  margin: 0 auto;
+  text-align: center;
+  word-break: keep-all;
+}
 
-  .hero {
-    text-align: center;
-
-    img {
-      max-width: 100%;
-      max-height: 280px;
-      display: block;
-      margin: 3rem auto 1.5rem;
-    }
-
-    h1 {
-      font-size: 3rem;
-    }
-
-    h1, .description, .action {
-      margin: 1.8rem auto;
-    }
-
-    .description {
-      max-width: 35rem;
-      font-size: 1.6rem;
-      line-height: 1.3;
-      color: lighten($textColor, 40%);
-    }
-
-    .action-button {
-      display: inline-block;
-      font-size: 1.2rem;
-      color: #fff;
-      background-color: $accentColor;
-      padding: 0.8rem 1.6rem;
-      border-radius: 4px;
-      transition: background-color 0.1s ease;
-      box-sizing: border-box;
-      border-bottom: 1px solid darken($accentColor, 10%);
-
-      &:hover {
-        background-color: lighten($accentColor, 10%);
-      }
-    }
-  }
-
-  .features {
-    border-top: 1px solid $borderColor;
-    padding: 1.2rem 0;
-    margin-top: 2.5rem;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: flex-start;
-    align-content: stretch;
+@media (max-width: 768px) {
+  .grid-cont {
+    --size: 150px;
+    gap: 15px;
+    font-size: 14px;
     justify-content: space-between;
+    line-height: 1.2;
   }
 
-  .feature {
-    flex-grow: 1;
-    flex-basis: 30%;
-    max-width: 30%;
-
-    h2 {
-      font-size: 1.4rem;
-      font-weight: 500;
-      border-bottom: none;
-      padding-bottom: 0;
-      color: lighten($textColor, 10%);
-    }
-
-    p {
-      color: lighten($textColor, 25%);
-    }
-  }
-
-  .footer {
-    padding: 2.5rem;
-    border-top: 1px solid $borderColor;
-    text-align: center;
-    color: lighten($textColor, 25%);
-  }
-}
-
-@media (max-width: $MQMobile) {
-  .home {
-    .features {
-      flex-direction: column;
-    }
-
-    .feature {
-      max-width: 100%;
-      padding: 0 2.5rem;
-    }
-  }
-}
-
-@media (max-width: $MQMobileNarrow) {
-  .home {
-    padding-left: 1.5rem;
-    padding-right: 1.5rem;
-
-    .hero {
-      img {
-        max-height: 210px;
-        margin: 2rem auto 1.2rem;
-      }
-
-      h1 {
-        font-size: 2rem;
-      }
-
-      h1, .description, .action {
-        margin: 1.2rem auto;
-      }
-
-      .description {
-        font-size: 1.2rem;
-      }
-
-      .action-button {
-        font-size: 1rem;
-        padding: 0.6rem 1.2rem;
-      }
-    }
-
-    .feature {
-      h2 {
-        font-size: 1.25rem;
-      }
-    }
+  .page-link .icon {
+    font-size: 1.8em;
   }
 }
 </style>
